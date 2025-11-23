146f138f629e81461416dda567159d0c65ed2bf2
_Benhoo <ahmed-jamal-dev>
2025-11-23 17:48:42 +0200

    login val

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -9,33 +9,39 @@ import {
 
 const registerVaildation = validationR;
 const loginVaildation = validationLogin;
-// router.post("/register", async (req, res) => {
-//   try {
-//     const { error, value } = registerVaildation.validate(req.body);
-//     if (error) {
-//       res.status(400).json({ message: error.message });
-//     }
-//     const userExist = await User.findByEmail(value.email);
-//     if (userExist) {
-//       res.status(404).json({ message: "user not founded" });
-//     }
+router.post("/register", async (req, res) => {
+  try {
+    const { error, value } = registerVaildation.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+    const userExist = await User.findByEmail(value.email);
+    if (userExist) {
+      res.status(404).json({ message: "user not founded" });
+    }
 
-//     const user = await User.create(value);
+    const user = await User.create(value);
 
-//     const token = user.generateToken();
-//     res.status(201).json({ message: "user created...", data: { token } });
-//   } catch (error) {}
-// });
+    const token = user.generateToken();
+    res.status(201).json({ message: "user created...", data: { token } });
+  } catch (error) {}
+});
 // LOGIN
 router.post("/login", async (req, res) => {
-      try {
-    const { email, password } = req.body;
+  try {
+    const { error, value } = loginVaildation.validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
 
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid email or password" });
 
-    const isMatch = await user.matchPassword(password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid email or password" });
+    const user = await User.findOne({ email: value.email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isMatch = await user.matchPassword(value.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = generateToken(user);
 
     return res.json({
       message: "Login successful",
@@ -45,13 +51,12 @@ router.post("/login", async (req, res) => {
         email: user.email,
         role: user.role,
       },
-      token: generateToken(user),
+      token,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-});
-//generateToken
+});//generateToken
 export const generateToken = (user) => {
   return jwt.sign(
     {
