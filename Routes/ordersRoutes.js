3b75c95eb99ef0f23b2dbe36c492077cedb5985e
mano2 <abdosobeih99@gmail.com>
2025-12-02 11:53:00 +0200

    adding get order by id and cancling order without deleting it

--- Routes/ordersRoutes.js
+++ Routes/ordersRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {  getMyOrders,createOrder,getAllOrders} from "../controllers/order.controller.js";
+import {  getMyOrders,createOrder,getAllOrders, getOrderById,cancelOrder} from "../controllers/order.controller.js";
 
 import { authMiddleware } from "../Middlewares/authMiddleware.js";
 import { authorizeRoles } from "../Middlewares/roleMiddleware.js";
@@ -10,6 +10,9 @@ const router = express.Router();
 router.post("/", authMiddleware, authorizeRoles("user"), createOrder);
 router.get("/my", authMiddleware, authorizeRoles("user"), getMyOrders);
 router.get("/", authMiddleware, authorizeRoles("admin"), getAllOrders);
+router.get("/:id", authMiddleware, authorizeRoles("user", "admin"), getOrderById);
+router.delete("/:id", authMiddleware, authorizeRoles("user"), cancelOrder);
+
 
 
 export default router;
--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -55,7 +55,57 @@ export const getMyOrders = async (req, res) => {
   }
 };
 
-export const getAllOrders = async (req, res, next) => {
+export const getOrderById = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId)
+      .populate("user", "name email")
+      .populate("items.product", "name price");
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    return res.status(200).json({ order });
+
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+export const cancelOrder = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    if (order.user.toString() !== userId) {
+      return res.status(403).json({ message: "Not allowed to cancel this order" });
+    }
+
+    if (order.orderStatus !== "Pending") {
+      return res.status(400).json({ message: "Order cannot be cancelled anymore" });
+    }
+
+    order.orderStatus = "Cancelled";
+    await order.save();
+
+    return res.json({ message: "Order cancelled successfully" });
+
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+
+
+export const getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find()
       .populate("user", "name email")
